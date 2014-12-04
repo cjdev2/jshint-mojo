757dbf6a2cb911d75e15616aa56cda15e5745ec7
aabdurrahman <aabdurrahman@cj.com>
2014-12-03 19:03:14 -0800

    Fix "java.lang.ClassCastException: org.mozilla.javascript.ConsString cannot be cast to java.lang.String"
    Rhino 1.7R4 uses an "optimized" string handling mechanism to improve performance - much like what Google V8 does.
    Developers will need to ensure that all concatenated JavaScript strings are fully "toString ()"ed before passing them back to Java.

--- src/main/java/com/cj/jshintmojo/jshint/JSHint.java
+++ src/main/java/com/cj/jshintmojo/jshint/JSHint.java
@@ -1,59 +1,58 @@
 package com.cj.jshintmojo.jshint;
 
-import java.io.InputStream;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.cj.jshintmojo.util.Rhino;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.CharEncoding;
 import org.mozilla.javascript.EcmaError;
 import org.mozilla.javascript.NativeArray;
 import org.mozilla.javascript.NativeObject;
 
-import com.cj.jshintmojo.util.Rhino;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JSHint {
-    
+
     private final Rhino rhino;
-    
-    public JSHint(String jshintCode) {
-        
-        rhino = new Rhino();
+
+    public JSHint (String jshintCode) {
+
+        rhino = new Rhino ();
         try {
-            rhino.eval(
-            		"print=function(){};" +
-            		"quit=function(){};" +
-            		"arguments=[];");
-            
-            rhino.eval(commentOutTheShebang(resourceAsString(jshintCode)));
+            rhino.eval (
+                    "print=function(){};" +
+                            "quit=function(){};" +
+                            "arguments=[];");
+
+            rhino.eval (commentOutTheShebang (resourceAsString (jshintCode)));
         } catch (EcmaError e) {
-            throw new RuntimeException("Javascript eval error:" + e.getScriptStackTrace(), e);
+            throw new RuntimeException ("Javascript eval error:" + e.getScriptStackTrace (), e);
         }
     }
 
-    private String commentOutTheShebang(String code) {
-        String minusShebang = code.startsWith("#!")?"//" + code : code;
+    private String commentOutTheShebang (String code) {
+        String minusShebang = code.startsWith ("#!") ? "//" + code : code;
         return minusShebang;
     }
 
-    public List<Error> run(InputStream source, String options, String globals) {
-        final List<Error> results = new ArrayList<JSHint.Error>();
+    public List<Error> run (InputStream source, String options, String globals) {
+        final List<Error> results = new ArrayList<JSHint.Error> ();
 
-        String sourceAsText = toString(source);
+        String sourceAsText = toString (source);
 
-        NativeObject nativeOptions = toJsObject(options);
-        NativeObject nativeGlobals = toJsObject(globals);
+        NativeObject nativeOptions = toJsObject (options);
+        NativeObject nativeGlobals = toJsObject (globals);
 
-        Boolean codePassesMuster = rhino.call("JSHINT", sourceAsText, nativeOptions, nativeGlobals);
+        Boolean codePassesMuster = rhino.call ("JSHINT", sourceAsText, nativeOptions, nativeGlobals);
 
-        if(!codePassesMuster){
-            NativeArray errors = rhino.eval("JSHINT.errors");
+        if (!codePassesMuster) {
+            NativeArray errors = rhino.eval ("JSHINT.errors");
 
-            for(Object next : errors){
-                if(next!=null){ // sometimes it seems that the last error in the list is null
-                    Error error = new Error(new JSObject(next));
-                    results.add(error);
+            for (Object next : errors) {
+                if (next != null) { // sometimes it seems that the last error in the list is null
+                    Error error = new Error (new JSObject (next));
+                    results.add (error);
                 }
             }
         }
@@ -61,80 +60,84 @@ public class JSHint {
         return results;
     }
 
-    private NativeObject toJsObject(String options) {
-        NativeObject nativeOptions = new NativeObject();
-        for (final String nextOption : options.split(",")) {
-            final String option = nextOption.trim();
-            if(!option.isEmpty()){
+    private NativeObject toJsObject (String options) {
+        NativeObject nativeOptions = new NativeObject ();
+        for (final String nextOption : options.split (",")) {
+            final String option = nextOption.trim ();
+            if (!option.isEmpty ()) {
                 final String name;
                 final Object value;
 
-                final int valueDelimiter = option.indexOf(':');
-                if(valueDelimiter==-1){
+                final int valueDelimiter = option.indexOf (':');
+                if (valueDelimiter == -1) {
                     name = option;
                     value = Boolean.TRUE;
                 } else {
-                    name = option.substring(0, valueDelimiter);
-                    String rest = option.substring(valueDelimiter+1).trim();
-                    if (rest.matches("[0-9]+")) {
-                        value = Integer.parseInt(rest);
-                    } else if (rest.equals("true")) {
+                    name = option.substring (0, valueDelimiter);
+                    String rest = option.substring (valueDelimiter + 1).trim ();
+                    if (rest.matches ("[0-9]+")) {
+                        value = Integer.parseInt (rest);
+                    } else if (rest.equals ("true")) {
                         value = Boolean.TRUE;
-                    } else if (rest.equals("false")) {
+                    } else if (rest.equals ("false")) {
                         value = Boolean.FALSE;
                     } else {
-                        value = rest;		        
+                        value = rest;
                     }
                 }
-                nativeOptions.defineProperty(name, value, NativeObject.READONLY);
+                nativeOptions.defineProperty (name, value, NativeObject.READONLY);
             }
         }
         return nativeOptions;
     }
 
-    private static String toString(InputStream in) {
+    private static String toString (InputStream in) {
         try {
-            return IOUtils.toString(in, CharEncoding.UTF_8);
+            return IOUtils.toString (in, CharEncoding.UTF_8);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException (e);
         }
     }
 
-    private String resourceAsString(String name) {
-        return toString(getClass().getResourceAsStream(name));
+    private String resourceAsString (String name) {
+        return toString (getClass ().getResourceAsStream (name));
     }
 
-    @SuppressWarnings("unchecked") 
+    @SuppressWarnings ("unchecked")
     static class JSObject {
         private NativeObject a;
 
-        public JSObject(Object o) {
-            if(o==null) throw new NullPointerException();
-            this.a = (NativeObject)o;
+        public JSObject (Object o) {
+            if (o == null) throw new NullPointerException ();
+            this.a = (NativeObject) o;
         }
 
-        public <T> T dot(String name){
-            return (T) a.get(name);
+        public <T> T dot (String name) {
+            return (T) a.get (name);
         }
     }
 
-    @SuppressWarnings("serial")
+    @SuppressWarnings ("serial")
     public static class Error implements Serializable {
         public String id, code, raw, evidence, reason;
         public Number line, character;
 
-        public Error(JSObject o) {
-            id = o.dot("id");
-            code = o.dot("code");
-            raw = o.dot("raw");
-            evidence = o.dot("evidence");
-            line = o.dot("line");
-            character = o.dot("character");
-            reason = o.dot("reason");
+        public Error (JSObject o) {
+            id = nullSafeToString (o, "id");
+            code = nullSafeToString (o, "code");
+            raw = nullSafeToString (o, "raw");
+            evidence = nullSafeToString (o, "evidence");
+            line = o.dot ("line");
+            character = o.dot ("character");
+            reason = nullSafeToString (o, "reason");
+        }
+
+        private String nullSafeToString (JSObject o, String name) {
+            return o.dot (name) != null ? o.dot (name).toString () : "";
         }
 
         // NOTE: for Unit Testing purpose.
-        public Error() {
+        public Error () {
         }
     }
 }
