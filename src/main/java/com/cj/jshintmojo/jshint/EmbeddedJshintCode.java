8c85c9de04744b2f45eee210c9a67c240a839bd2
Davide Angelocola <davide.angelocola@exmachina.ch>
2016-01-02 22:46:54 +0100

    dev: adding jshint 2.9.0

--- src/main/java/com/cj/jshintmojo/jshint/EmbeddedJshintCode.java
+++ src/main/java/com/cj/jshintmojo/jshint/EmbeddedJshintCode.java
@@ -7,6 +7,7 @@ import java.util.Map;
 public class EmbeddedJshintCode {
     public static final Map <String, String> EMBEDDED_VERSIONS =
             Collections.unmodifiableMap (new HashMap <String, String> () {{
+                put ("2.9.0", "jshint-rhino-2.9.0.js");
                 put ("2.5.6", "jshint-rhino-2.5.6.js");
                 put ("2.4.3", "jshint-rhino-2.4.3.js");
                 put ("2.4.1", "jshint-rhino-2.4.1.js");
