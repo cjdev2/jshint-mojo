62a3a719f9d41f1b885e236ab37b12bc02b132db
Fabiano C. de Oliveira <fabcipriano@yahoo.com.br>
2017-05-13 23:20:39 -0300

    Update plugin to use JSHint 2.9.4. FIX JSHint javascript to run in Rhino enviroment

--- src/main/java/com/cj/jshintmojo/jshint/EmbeddedJshintCode.java
+++ src/main/java/com/cj/jshintmojo/jshint/EmbeddedJshintCode.java
@@ -6,7 +6,8 @@ import java.util.Map;
 
 public class EmbeddedJshintCode {
     public static final Map <String, String> EMBEDDED_VERSIONS =
-            Collections.unmodifiableMap (new HashMap <String, String> () {{
+            Collections.unmodifiableMap (new HashMap <String, String> () {{                
+                put ("2.9.4", "jshint-2.9.4.js");
                 put ("2.5.6", "jshint-rhino-2.5.6.js");
                 put ("2.4.3", "jshint-rhino-2.4.3.js");
                 put ("2.4.1", "jshint-rhino-2.4.1.js");
